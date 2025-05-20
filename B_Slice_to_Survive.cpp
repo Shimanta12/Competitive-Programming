f45b4da282af476b3c1e1b4ce41cb03119f5e06b
Emon Barua Shimanta <sbemon1356@gmail.com>
2025-05-20 20:55:15 +0600

    first commit

---
+++ B_Slice_to_Survive.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1000000007;
+const int INF = 2e9;
+const double PI = acos(-1);
+
+int d[8][2] = {{0, -1}, {0, 1}, { -1, 0}, {1, 0}, { -1, -1}, { -1, 1}, {1, 1}, {1, -1}};
+
+#define ll             long long
+
+#define endl           "\n"
+
+#define vi             vector<int>
+#define pb             push_back
+#define all(x)         begin(x), end(x)
+#define sz(x)          (int)(x).size()
+
+#define pi             pair<int, int>
+#define F              first
+#define S              second
+#define mp             make_pair
+
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+void solve(int tc) {
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+    vector<pair<int, int>> opts = {
+        {a, m}, {n-a+1, m}, {n, b} , {n, m-b+1}
+    };
+    int ans = INT_MAX;
+    for(auto opt : opts){
+        n = opt.F;
+        m = opt.S;
+        int cnt1 = 0;
+        while(n > 1){
+            n = (n+1)/2;
+            cnt1++;
+        }
+        int cnt2 = 0;
+        while(m > 1){
+            m = (m+1)/2;
+            cnt2++;
+        }
+        ans = min(ans, cnt1+cnt2);
+    }
+    cout << ans + 1 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc = 1;
+    
+    cin >> tc;
+    for (int i = 1; i <= tc; i++) {
+        solve(i);
+    }
+
+    return 0;
+}

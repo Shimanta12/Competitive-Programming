36b3b01794fb871bebe2bccd7c7c8dfeb58593b7
Emon Barua Shimanta <86702731+Shimanta12@users.noreply.github.com>
2025-06-10 00:38:49 +0600

    Add solutions to Codeforces Round 1029 (Div. 3)(A,B,C,D)

---
+++ Codeforces Round 1029 (Div. 3)/C_Cool_Partition.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1000000007;
+const int INF = 2e9;
+const double PI = acos(-1);
+
+int d[8][2] = {{0, -1}, {0, 1}, { -1, 0}, {1, 0}, { -1, -1}, { -1, 1}, {1, 1}, {1, -1}};
+
+#define ll             long long
+
+#define endl           "\n"
+
+#define vi             vector<int>
+#define pb             push_back
+#define all(x)         begin(x), end(x)
+#define sz(x)          (int)(x).size()
+
+#define pi             pair<int, int>
+#define F              first
+#define S              second
+#define mp             make_pair
+
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+void solve(int tc) {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+    int ans = 1;
+    set<int> s1 = {v[0]};
+    set<int> s2;
+    for(int i=1; i<n;){
+        if(s1.empty()){
+            while(!s2.empty()){
+                s1.insert(*s2.begin());
+                s2.erase(s2.begin());
+            }
+        }
+        int j=i;
+        while(j < n && !s1.empty()){
+            if(s1.find(v[j]) != s1.end()){
+                s2.insert(v[j]);
+                s1.erase(v[j]);
+            }else{
+                s2.insert(v[j]);
+            }
+            j++;
+        }
+        if(s1.empty()){
+            ans++;
+        }
+        i = j;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc = 1;
+    
+    cin >> tc;
+    for (int i = 1; i <= tc; i++) {
+        solve(i);
+    }
+
+    return 0;
+}

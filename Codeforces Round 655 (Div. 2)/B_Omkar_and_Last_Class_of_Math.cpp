e3919927e1b3d263aa826292656c85a05e516181
Emon Barua Shimanta <86702731+Shimanta12@users.noreply.github.com>
2025-07-01 10:49:18 +0600

    Add solution to Codeforces Round 655 (Div. 2) (B)

---
+++ Codeforces Round 655 (Div. 2)/B_Omkar_and_Last_Class_of_Math.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1000000007;
+const int INF = 2e9;
+const double PI = acos(-1);
+
+int d[8][2] = {{0, -1}, {0, 1}, { -1, 0}, {1, 0}, { -1, -1}, { -1, 1}, {1, 1}, {1, -1}};
+
+#define ll             long long
+
+#define endl           "\n"
+
+#define vi             vector<int>
+#define pb             push_back
+#define all(x)         begin(x), end(x)
+#define sz(x)          (int)(x).size()
+
+#define pi             pair<int, int>
+#define F              first
+#define S              second
+#define mp             make_pair
+
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+void solve(int tc) {
+    int n;
+    cin >> n;
+    int mx = 1;
+    for(int i=2; i*i<=n; i++){
+        if(n%i == 0){
+            mx = max(mx, i);
+            mx = max(mx, n/i);
+        }
+    }
+    cout << mx << " " << n - mx << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc = 1;
+    
+    cin >> tc;
+    for (int i = 1; i <= tc; i++) {
+        solve(i);
+    }
+
+    return 0;
+}

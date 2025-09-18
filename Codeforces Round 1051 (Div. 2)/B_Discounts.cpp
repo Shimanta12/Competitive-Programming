a3d533b5cc782f14072c4826ffac47ba120d1e87
Emon Barua Shimanta <86702731+Shimanta12@users.noreply.github.com>
2025-09-18 11:50:05 +0600

    Add solution to Codeforces Round 1051(Div. 2) (A,B)

---
+++ Codeforces Round 1051 (Div. 2)/B_Discounts.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1000000007;
+const int INF = 2e9;
+const double PI = acos(-1);
+
+int d[8][2] = {{0, -1}, {0, 1}, { -1, 0}, {1, 0}, { -1, -1}, { -1, 1}, {1, 1}, {1, -1}};
+
+#define ll             long long
+
+#define endl           "\n"
+
+#define vi             vector<int>
+#define pb             push_back
+#define all(x)         begin(x), end(x)
+#define sz(x)          (int)(x).size()
+
+#define pi             pair<int, int>
+#define F              first
+#define S              second
+#define mp             make_pair
+
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+void solve(int tc) {
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    vector<int> b(k);
+    
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+    for(int i=0; i<k; i++){
+        cin >> b[i];
+    }
+    sort(all(v), greater<int>());
+    vector<int> a(n+1);
+    for(int i=0; i<n; i++){
+        a[i+1] = v[i];
+    }
+
+    vector<ll> prefix(n+1);
+    prefix[0] = 0;
+    for(int i=1; i<=n; i++){
+        prefix[i] = prefix[i-1] + a[i];
+    }
+    sort(all(b));
+    ll ans = 0;
+    int curr = 1;
+    for(int i=0; i<k; i++){
+        if((curr + b[i] - 1) > n) break;
+        if(b[i] == 1){
+            curr++;
+        }else{
+            ans += prefix[curr + b[i] - 1] - prefix[curr - 1] - a[curr + b[i] - 1];
+            curr =  curr + b[i];
+        }
+    }
+    if(curr <= n){
+        ans += prefix[n] - prefix[curr - 1];
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc = 1;
+    
+    cin >> tc;
+    for (int i = 1; i <= tc; i++) {
+        solve(i);
+    }
+
+    return 0;
+}

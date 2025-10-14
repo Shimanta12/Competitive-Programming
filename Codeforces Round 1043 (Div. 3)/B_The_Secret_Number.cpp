fe9b0c87402a7101b2c72dc5fd7b19cd8b8324d4
Emon Barua Shimanta <86702731+Shimanta12@users.noreply.github.com>
2025-10-14 13:24:08 +0600

    Add solutions to Codeforces Round 1043 (Div. 3) (A, B, C_1, C_2)

---
+++ Codeforces Round 1043 (Div. 3)/B_The_Secret_Number.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1000000007;
+const int INF = 2e9;
+const double PI = acos(-1);
+
+int d[8][2] = {{0, -1}, {0, 1}, { -1, 0}, {1, 0}, { -1, -1}, { -1, 1}, {1, 1}, {1, -1}};
+
+#define int             long long
+
+#define endl           "\n"
+
+#define vi             vector<int>
+#define pb             push_back
+#define all(x)         begin(x), end(x)
+#define sz(x)          (int)(x).size()
+
+#define pi             pair<int, int>
+#define F              first
+#define S              second
+
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+void solve(int tc) {
+    int n;
+    cin >> n;
+    int p = 1;
+    vector<int> ans;
+    for(int i=1; i<=18; i++){
+        p *= 10;
+        if(n % (1+p) == 0){
+            ans.pb(n/(1+p));
+        }
+    }
+    sort(all(ans));
+    cout << ans.size() << endl;
+    for(int e : ans){
+        cout << e << " ";
+    }
+    if(ans.size()){
+        cout << endl;
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc = 1;
+    
+    cin >> tc;
+    for (int i = 1; i <= tc; i++) {
+        solve(i);
+    }
+
+    return 0;
+}
